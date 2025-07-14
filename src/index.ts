dbe1960a080262a2244a927c8b385bc4a6c9e968
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-07-14 11:29:14 +0300

    ♻️ refactor(index): simplify app initialization by delegating to `app.ts`
    - Moved Express app setup to `app.ts` for cleaner index structure
    - Updated `index.ts` to import and utilize the centralized `app` instance
    
    Improves modularity and reduces redundancy in application setup.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import express from 'express';
-import smsRoutes from './routes/SmsRoutes';
-import {config} from './config/env';
-
-const app = express();
-app.use(express.json());
-app.use('/api/v1', smsRoutes);
+import {config} from './config/env'
+import app from './app'
 
 app.listen(config.port, () => {
     console.log(`🚀 Running at http://127.0.0.1:${config.port}/api/v1 [${config.env}]`);
 });
+

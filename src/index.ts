dfaca02df0e5dbb97e66bc320a14664c396c163b
masgeek <barsamms@gmail.com>
2026-07-21 15:37:28 +0300

    chore(build): update versioning script, tsconfig rootDir, and package version handling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import {config} from './config/env'
 import app from './app'
 import {logger} from './utils/logger'
-import {version} from '../package.json'
+import fs from 'fs'
+import path from 'path'
+
+const version = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
+).version;
 
 const server = app.listen(config.port, () => {
     logger.info(`Running at http://127.0.0.1:${config.port}/api/v1 [${config.env}] v${version}`);
